d6dff09f365f2717b7f9f8dc2558b515469c8667
Harvey Sanders <sanders.harvey@gmail.com>
2022-02-25 15:52:35 -0500

    Posting to Greenlight

--- function.go
+++ function.go
@@ -15,67 +15,70 @@ var SLACK_WEBHOOK_URL = os.Getenv("SLACK_WEBHOOK_URL")
 var decoder = schema.NewDecoder()
 
 // handleJson unmarshalls a JSON payload from a signUp request into a Signup.
-func handleJson(w http.ResponseWriter, r *http.Request) (Signup, error) {
+func handleJson(s *Signup, r *http.Request) error {
 	b, err := io.ReadAll(r.Body)
-	s := Signup{}
 	if err != nil {
-		return s, err
+		return err
 	}
 
-	err = json.Unmarshal(b, &s)
+	err = json.Unmarshal(b, s)
 	if err != nil {
-		return s, err
+		return err
 	}
 
-	return s, nil
+	return nil
 }
 
 // handleForm unmarshalls a FormData payload from a signUp request into a Signup
-func handleForm(w http.ResponseWriter, r *http.Request) (Signup, error) {
-	s := Signup{}
+func handleForm(s *Signup, r *http.Request) error {
 	err := r.ParseForm()
 	if err != nil {
-		return s, err
+		return err
 	}
 
-	err = decoder.Decode(&s, r.PostForm)
+	err = decoder.Decode(s, r.PostForm)
 	if err != nil {
-		return s, err
+		return err
 	}
 
-	return s, nil
+	return nil
 }
 
 // HandleSignUp parses Info Session sign up requests from operationspark.org.
 // If successful, it sends webhooks to Greenlight, Slack, other services.
 func HandleSignUp(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(SLACK_WEBHOOK_URL)
-	slackMsg := slack.Message{}
+	s := Signup{}
 
 	switch r.Header.Get("Content-Type") {
 	case "application/json":
-		s, err := handleJson(w, r)
+		err := handleJson(&s, r)
 		if err != nil {
 			http.Error(w, "Error reading sign up body", http.StatusInternalServerError)
 			panic(err)
 		}
-		slackMsg = slack.Message{Text: s.Summary()}
 
 	case "application/x-www-form-urlencoded":
-		s, err := handleForm(w, r)
+		err := handleForm(&s, r)
 		if err != nil {
 			http.Error(w, "Error reading Form Body", http.StatusInternalServerError)
 			panic(err)
 		}
 		fmt.Println(s)
-		slackMsg = slack.Message{Text: s.Summary()}
 
 	default:
 		http.Error(w, "Unacceptable Content-Type", http.StatusUnsupportedMediaType)
 		return
 	}
 
-	err := slack.SendWebhook(SLACK_WEBHOOK_URL, slackMsg)
+	err := s.SignUp()
+	if err != nil {
+		http.Error(w, "Error sending webhook to Greenlight", http.StatusInternalServerError)
+		panic(err)
+	}
+
+	slackMsg := slack.Message{Text: s.Summary()}
+	err = slack.SendWebhook(SLACK_WEBHOOK_URL, slackMsg)
 	if err != nil {
 		http.Error(w, "Error sending Slack webhook", http.StatusInternalServerError)
 		panic(err)
