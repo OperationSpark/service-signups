da9174a35b7aca3cbfd659eb56dab39fd5dc7a1d
Harvey Sanders <sanders.harvey@gmail.com>
2022-02-25 10:24:11 -0500

    Use legacy signup payload fields

--- signup.go
+++ signup.go
@@ -3,30 +3,27 @@ package signup
 import (
 	"fmt"
 	"strings"
+	"time"
 )
 
-type Session struct {
-	Id string `json:"id"`
-}
-
-type Referral struct {
-	Value          string `json:"value"`
-	AdditionalInfo string `json:"additionalInfo"`
-}
-
 type SignUp struct {
-	Session      Session  `json:"session"`
-	Email        string   `json:"email"`
-	FirstName    string   `json:"firstName"`
-	LastName     string   `json:"lastName"`
-	Phone        string   `json:"phone"`
-	ReferencedBy Referral `json:"referencedBy"`
+	ProgramId        string    `json:"programId" schema:"programId"`
+	NameFirst        string    `json:"nameFirst" schema:"nameFirst"`
+	NameLast         string    `json:"nameLast" schema:"nameLast"`
+	Email            string    `json:"email" schema:"email"`
+	Cell             string    `json:"cell" schema:"cell"`
+	Referrer         string    `json:"referrer" schema:"referrer"`
+	ReferrerResponse string    `json:"referrerResponse" schema:"referrerResponse"`
+	StartDateTime    time.Time `json:"startDateTime" schema:"startDateTime"`
+	Cohort           string    `json:"cohort" schema:"cohort"`
+	SessionId        string    `json:"sessionId" schema:"sessionId"`
+	Token            string    `json:"token" schema:"token"`
 }
 
 func (s *SignUp) Summary() string {
 	msg := strings.Join([]string{
-		fmt.Sprintf("%s %s has signed up for %s", s.FirstName, s.LastName, s.Session.Id),
-		fmt.Sprintf("Ph: %s)", s.Phone),
+		fmt.Sprintf("%s %s has signed up for %s", s.NameFirst, s.NameLast, s.Cohort),
+		fmt.Sprintf("Ph: %s)", s.Cell),
 		fmt.Sprintf("email: %s)", s.Email),
 	}, "\n")
 	return msg
