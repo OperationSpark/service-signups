5d91d55ed39bdcbf5e9e240762674325ed649460
Harvey Sanders <sanders.harvey@gmail.com>
2022-12-12 15:11:26 -0500

    Update Greenlight test

--- greenlight_test.go
+++ greenlight_test.go
@@ -16,16 +16,17 @@ func TestPostWebhook(t *testing.T) {
 
 		sessionStartDate, _ := time.Parse(time.RFC822, "14 Mar 22 17:00 UTC")
 		su := Signup{
-			NameFirst:        "Bob",
-			NameLast:         "Ross",
-			Email:            "bross@pbs.org",
-			Cell:             "555-123-4567",
-			Referrer:         "instagram",
-			ReferrerResponse: "",
-			StartDateTime:    sessionStartDate,
-			Cohort:           "is-mar-14-22-12pm",
-			SessionID:        "X5TsABhN94yesyMEi",
-			UserLocation:     "Louisiana",
+			NameFirst:         "Bob",
+			NameLast:          "Ross",
+			Email:             "bross@pbs.org",
+			Cell:              "555-123-4567",
+			Referrer:          "instagram",
+			ReferrerResponse:  "",
+			StartDateTime:     sessionStartDate,
+			Cohort:            "is-mar-14-22-12pm",
+			SessionID:         "X5TsABhN94yesyMEi",
+			UserLocation:      "Louisiana",
+			AttendingLocation: "IN_PERSON",
 		}
 		mockGreenlightSvr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			// Sends API Key header
