476152302d2b9fa361d947402b268f3b4e5fe5fe
Harvey Sanders <sanders.harvey@gmail.com>
2023-01-09 15:25:13 -0500

    Smoke test setup

---
+++ cmd/smoke/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("***** TODO: ******")
+
+	// TODO: Fetch Info Session ID from Greenlight endpoint
+	// TODO: POST Signup to production endpoint
+	// TODO: Get expected SMS message from Twilio API
+	// TODO: Parse info link in SMS and visit it
+	// TODO: Ensure rendered page has expected content
+}
