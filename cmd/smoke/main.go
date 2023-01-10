d2a50f79328cafcd50738548d811d176022cf4f4
Harvey Sanders <sanders.harvey@gmail.com>
2023-01-10 02:02:40 -0500

    Authenticated HTTP trigger

--- cmd/smoke/main.go
+++ cmd/smoke/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"time"
 
 	signup "github.com/operationspark/service-signup"
+	"google.golang.org/api/idtoken"
 )
 
 type (
@@ -89,12 +92,18 @@ func (s *smoke) postSignup() error {
 	}
 	var body bytes.Buffer
 	e := json.NewEncoder(&body)
-	err := e.Encode(su)
+	err := e.Encode(&su)
 	if err != nil {
 		return fmt.Errorf("encode: %w", err)
 	}
-	// TODO: Use Google Auth to trigger cloud function
-	resp, err := http.Post(s.signupAPIurl, "application/json", &body)
+
+	// Use Google Auth to trigger cloud function
+	req, err := makeAuthenticatedReq(http.MethodPost, s.signupAPIurl, &body)
+	if err != nil {
+		return fmt.Errorf("auth'd req: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return fmt.Errorf("http POST: %w", err)
 	}
@@ -104,4 +113,22 @@ func (s *smoke) postSignup() error {
 		return fmt.Errorf("http error: %w", err)
 	}
 	return nil
+}
+
+// MakeAuthenticatedReq makes an HTTP request using Google Service Account credentials.
+func makeAuthenticatedReq(method string, url string, body io.Reader) (*http.Request, error) {
+	audience := url
+	creds := os.Getenv("GCP_SA_CREDS_JSON")
+	opts := idtoken.WithCredentialsJSON([]byte(creds))
+	ts, err := idtoken.NewTokenSource(context.Background(), audience, opts)
+	if err != nil {
+		return nil, fmt.Errorf("newTokenSource: %w", err)
+	}
+	token, err := ts.Token()
+	if err != nil {
+		return nil, fmt.Errorf("token: %w", err)
+	}
+	req, err := http.NewRequest(method, audience, body)
+	token.SetAuthHeader(req)
+	return req, err
 }
