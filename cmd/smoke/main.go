5862248743382beedc2f903775ab8d1124eb17a1
Harvey Sanders <sanders.harvey@gmail.com>
2023-01-10 16:24:14 -0500

    Initial smoke test working

--- cmd/smoke/main.go
+++ cmd/smoke/main.go
@@ -7,11 +7,17 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"regexp"
+	"strings"
 	"time"
 
 	signup "github.com/operationspark/service-signup"
+	"github.com/operationspark/service-signup/greenlight"
+	"github.com/twilio/twilio-go"
+	twiAPI "github.com/twilio/twilio-go/rest/api/v2010"
 	"google.golang.org/api/idtoken"
 )
 
@@ -27,11 +33,17 @@ type (
 		toEmail string
 		// Twilio number that accepts test SMS messages.
 		toNum string
+		// Twilio number the signup service uses to sends SMS messages.
+		fromNum string
 	}
 
 	openSession struct {
-		ID           string `json:"_id"`
-		LocationType string `json:"locationType"`
+		ID           string                 `json:"_id"`
+		Cohort       string                 `json:"cohort"`
+		LocationType string                 `json:"locationType"`
+		GooglePlace  greenlight.GooglePlace `json:"googlePlace"`
+		Private      bool                   `json:"private"`
+		ProgramID    string                 `json:"programId"`
 		Times        struct {
 			Start struct {
 				DateTime time.Time `json:"dateTime"`
@@ -48,6 +60,7 @@ func newSmokeTest() *smoke {
 		signupAPIurl: "https://us-central1-operationspark-org.cloudfunctions.net/session-signups",
 		toEmail:      os.Getenv("TEST_TO_EMAIL"),
 		toNum:        os.Getenv("TEST_TO_NUM"),
+		fromNum:      os.Getenv("TWILIO_PHONE_NUMBER"),
 	}
 }
 
@@ -55,7 +68,7 @@ func (s *smoke) fetchInfoSessions() error {
 	type response struct {
 		Sessions []openSession `json:"sessions"`
 	}
-	resp, err := http.Get(s.glAPIurl + "/sessions/open?programId=5sTmB97DzcqCwEZFR&limit=1")
+	resp, err := http.Get(s.glAPIurl + "/sessions/open?programId=5sTmB97DzcqCwEZFR&limit=4")
 	if err != nil {
 		return fmt.Errorf("GET: %w", err)
 	}
@@ -67,29 +80,29 @@ func (s *smoke) fetchInfoSessions() error {
 		return fmt.Errorf("decode: %w", err)
 	}
 
-	if resp.StatusCode >= 400 {
-		signup.HandleHTTPError(resp)
-		return fmt.Errorf("http error: %s", resp.Status)
+	err = checkHTTPError(resp)
+	if err != nil {
+		return err
 	}
 
-	if len(respBody.Sessions) == 0 {
-		return errors.New("no open Info Sessions provided from Greenlight to select")
+	for _, session := range respBody.Sessions {
+		// Select next upcoming session that isn't 'private' (test)
+		if session.Private {
+			continue
+		}
+		s.selectedSession = session
+		return nil
 	}
-
-	// Select next upcoming session
-	s.selectedSession = respBody.Sessions[0]
-	return nil
+	return errors.New("no open Info Sessions provided from Greenlight to select")
 }
 
-func (s *smoke) postSignup() error {
-	su := signup.Signup{
-		NameFirst:     "Halle",
-		NameLast:      "Bot",
-		Email:         s.toEmail,
-		Cell:          s.toNum,
-		SessionID:     s.selectedSession.ID,
-		StartDateTime: s.selectedSession.Times.Start.DateTime,
+func (s *smoke) postSignup(su signup.Signup) error {
+	if os.Getenv("DRY_RUN") != "" {
+		fmt.Printf("Signup: %+v", su)
+		fmt.Printf("** Dry Run **\nSkipping Signup POST\n")
+		return nil
 	}
+
 	var body bytes.Buffer
 	e := json.NewEncoder(&body)
 	err := e.Encode(&su)
@@ -108,11 +121,7 @@ func (s *smoke) postSignup() error {
 		return fmt.Errorf("http POST: %w", err)
 	}
 
-	err = signup.HandleHTTPError(resp)
-	if err != nil {
-		return fmt.Errorf("http error: %w", err)
-	}
-	return nil
+	return checkHTTPError(resp)
 }
 
 // MakeAuthenticatedReq makes an HTTP request using Google Service Account credentials.
@@ -120,6 +129,10 @@ func makeAuthenticatedReq(method string, url string, body io.Reader) (*http.Requ
 	audience := url
 	creds := os.Getenv("GCP_SA_CREDS_JSON")
 	opts := idtoken.WithCredentialsJSON([]byte(creds))
+
+	if creds == "" {
+		opts = idtoken.WithCredentialsFile("../../creds.json")
+	}
 	ts, err := idtoken.NewTokenSource(context.Background(), audience, opts)
 	if err != nil {
 		return nil, fmt.Errorf("newTokenSource: %w", err)
@@ -131,4 +144,106 @@ func makeAuthenticatedReq(method string, url string, body io.Reader) (*http.Requ
 	req, err := http.NewRequest(method, audience, body)
 	token.SetAuthHeader(req)
 	return req, err
+}
+
+func fetchSMSmessage(toNum, fromNum string) (string, error) {
+	accountSID := os.Getenv("TWILIO_ACCOUNT_SID")
+
+	client := twilio.NewRestClientWithParams(twilio.ClientParams{
+		Username: accountSID,
+		Password: os.Getenv("TWILIO_AUTH_TOKEN"),
+	})
+
+	params := &twiAPI.ListMessageParams{}
+	params.SetPathAccountSid(accountSID)
+	params.SetTo(toNum)
+	params.SetFrom(fromNum)
+	params.SetLimit(1)
+
+	messages, err := client.Api.ListMessage(params)
+	if err != nil {
+		return "", fmt.Errorf("fetchMessage: %w", err)
+	}
+	if len(messages) == 0 {
+		return "", fmt.Errorf("no messages found sent from %q -> %q", fromNum, toNum)
+	}
+
+	return *messages[0].Body, nil
+}
+
+// ParseSMSShortLink pulls a "ospk.org" short link out of a string.
+func parseSMSShortLink(sms string) string {
+	re := regexp.MustCompile(`https://ospk\.org/\w{10}`)
+	match := re.FindString(sms)
+	return match
+}
+
+// ParseSMSOriginal pulls an Info Session info link from an SMS body.
+// This func is used to check if the URL shortener service has failed/
+func parseSMSOriginalLink(sms string) string {
+	// https://sms.operationspark.org/
+	re := regexp.MustCompile(`https://sms\.operationspark\.org/m/\w+={1,2}`)
+	match := re.FindString(sms)
+	return match
+}
+
+// FetchLinkBody GETs a link and returns the body or error if HTTP response is 400+.
+func fetchLinkBody(link string) (io.ReadCloser, error) {
+	resp, err := http.Get(link)
+	if err != nil {
+		return resp.Body, fmt.Errorf("GET: %w", err)
+	}
+
+	err = checkHTTPError(resp)
+	return resp.Body, err
+}
+
+func checkInfoPageContent(body io.Reader, wantStrings ...string) error {
+	b, err := ioutil.ReadAll(body)
+	if err != nil {
+		return fmt.Errorf("readAll: %w", err)
+	}
+
+	for _, targetVal := range wantStrings {
+		found := bytes.Contains(b, []byte(targetVal))
+		if !found {
+			htmlBody := extractHTMLBody(b)
+			return fmt.Errorf("target value %q not found in Info Page body:\n%s", targetVal, string(htmlBody))
+		}
+	}
+	return nil
+}
+
+// checkHTTPError checks if HTTP status abd returns an error if code is >= 400.
+func checkHTTPError(resp *http.Response) error {
+	if resp.StatusCode < 400 {
+		return nil
+	}
+	reqLabel := fmt.Sprintf(
+		"%s: %s://%s\n%s\n",
+		resp.Request.Method,
+		resp.Request.URL.Scheme,
+		resp.Request.URL.Host,
+		resp.Request.URL.RequestURI(),
+	)
+
+	errMsg := fmt.Sprintf("HTTP Error:\n%s\nResponse:\n%s", reqLabel, resp.Status)
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("readAll: %w", err)
+	}
+
+	isHTML := strings.Contains(resp.Header.Get("Content-Type"), "text/html")
+	if isHTML {
+		body := extractHTMLBody(body)
+		return fmt.Errorf("%s\n\n%s", errMsg, body)
+	}
+
+	return fmt.Errorf("%s\n%s", errMsg, body)
+}
+
+func extractHTMLBody(html []byte) []byte {
+	re := regexp.MustCompile(`<body>(.+)</body>`)
+	return re.Find(html)
 }
--- cmd/smoke/smoke_test.go
+++ cmd/smoke/smoke_test.go
@@ -4,7 +4,10 @@ import (
 	"os"
 	"strings"
 	"testing"
+	"time"
 
+	signup "github.com/operationspark/service-signup"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -21,11 +24,102 @@ func TestSmokeSignup(t *testing.T) {
 	require.NoError(t, err, "fetching open sessions")
 
 	// POST Signup to production endpoint
-	err = s.postSignup()
+	su := signup.Signup{
+		AttendingLocation: "IN_PERSON",
+		Cell:              s.toNum,
+		Cohort:            s.selectedSession.Cohort,
+		Email:             s.toEmail,
+		GooglePlace:       s.selectedSession.GooglePlace,
+		LocationType:      s.selectedSession.LocationType,
+		NameFirst:         "Halle",
+		NameLast:          "Bot",
+		ProgramID:         s.selectedSession.ProgramID,
+		Referrer:          "verbal",
+		ReferrerResponse:  "Automated Smoke Test",
+		SessionID:         s.selectedSession.ID,
+		StartDateTime:     s.selectedSession.Times.Start.DateTime,
+		UserLocation:      "South Dakota",
+	}
+
+	// Conditionally set AttendingLocation based on LocationType
+	if su.LocationType == "HYBRID" || su.LocationType == "VIRTUAL" {
+		su.AttendingLocation = "VIRTUAL"
+	}
+
+	// TODO: Also test "None of these fit my schedule" option
+	err = s.postSignup(su)
 	require.NoError(t, err, "POST Signup to Cloud Function")
-	// TODO: Get expected SMS message from Twilio API
-	// TODO: Parse info link in SMS and visit it
+
+	// Get expected SMS message from Twilio API
+	sms, err := fetchSMSmessage(s.toNum, s.fromNum)
+	require.NoError(t, err)
+	require.NotEmptyf(t, sms, "no message found sent from %q -> %q", s.fromNum, s.toNum)
+
+	// Parse info link in SMS
+	link := parseSMSShortLink(sms)
+	// Intentionally using assert to continue running tests even if shortener fails
+	assert.NotEmpty(t, link, "URL Shortener service failed\nSMS: %q", sms)
+
+	if link == "" {
+		// Shortener service failed, get the long link
+		link = parseSMSOriginalLink(sms)
+	}
+
+	// Visit link
+	body, err := fetchLinkBody(link)
+	require.NoError(t, err)
+
 	// TODO: Ensure rendered page has expected content
+	ct, err := time.LoadLocation("america/chicago")
+	require.NoError(t, err)
+
+	err = checkInfoPageContent(body,
+		// Session Date
+		s.selectedSession.Times.Start.DateTime.In(ct).Format("Monday, January 2, 2006"),
+		// Session Time
+		s.selectedSession.Times.Start.DateTime.In(ct).Format("3:00pm (MST)"),
+		// TODO:
+		// Hello Name
+		// Zoom link
+		// conditionally check for location (HYBRID || VIRTUAL)
+	)
+	require.NoError(t, err)
+}
+
+func TestLinkExtractors(t *testing.T) {
+	t.Run("extracts short link from SMS", func(t *testing.T) {
+		sms := `You've signed up for an info session with Operation Spark! The session is Mon Jan 09 @ 12:00p CST. View this link for details: https://ospk.org/lztvCQIvUK`
+
+		want := "https://ospk.org/lztvCQIvUK"
+		got := parseSMSShortLink(sms)
+		require.Equal(t, want, got)
+	})
+
+	t.Run("extracts long link when shortener service fails", func(t *testing.T) {
+		sms := `You've signed up for an info session with Operation Spark! The session is Mon Jan 09 @ 12:00p CST. View this link for details: https://sms.operationspark.org/m/eyJ0ZW1wbGF0ZSI6IkluZm9TZXNzaW9uIiwiem9vbUxpbmsiOiJodHRwczovL3VzMDZ3ZWIuem9vbS51cy93L0FBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBIiwiZGF0ZSI6IjIwMjMtMDEtMDJUMTg6MDA6MDBaIiwibmFtZSI6IkhhbGxlIiwibG9jYXRpb25UeXBlIjoiSFlCUklEIiwibG9jYXRpb24iOnsibmFtZSI6Ik9wZXJhdGlvbiBTcGFyayIsImxpbmUxIjoiNTE0IEZyYW5rbGluIEF2ZSIsImNpdHlTdGF0ZVppcCI6Ik5ldyBPcmxlYW5zLCBMQSA3MDExNyIsIm1hcFVybCI6Imh0dHBzOi8vd3d3Lmdvb2dsZS5jb20vbWFwcy9wbGFjZS81MTQrRnJhbmtsaW4rQXZlJTJDTmV3K09ybGVhbnMlMkMrTEErNzAxMTcifX0=`
+
+		want := "https://sms.operationspark.org/m/eyJ0ZW1wbGF0ZSI6IkluZm9TZXNzaW9uIiwiem9vbUxpbmsiOiJodHRwczovL3VzMDZ3ZWIuem9vbS51cy93L0FBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBIiwiZGF0ZSI6IjIwMjMtMDEtMDJUMTg6MDA6MDBaIiwibmFtZSI6IkhhbGxlIiwibG9jYXRpb25UeXBlIjoiSFlCUklEIiwibG9jYXRpb24iOnsibmFtZSI6Ik9wZXJhdGlvbiBTcGFyayIsImxpbmUxIjoiNTE0IEZyYW5rbGluIEF2ZSIsImNpdHlTdGF0ZVppcCI6Ik5ldyBPcmxlYW5zLCBMQSA3MDExNyIsIm1hcFVybCI6Imh0dHBzOi8vd3d3Lmdvb2dsZS5jb20vbWFwcy9wbGFjZS81MTQrRnJhbmtsaW4rQXZlJTJDTmV3K09ybGVhbnMlMkMrTEErNzAxMTcifX0="
+		got := parseSMSOriginalLink(sms)
+		require.Equal(t, want, got)
+	})
+}
+
+func TestCheckInfoPageContent(t *testing.T) {
+	t.Run("returns no error if all the target strigns are found", func(t *testing.T) {
+		html := `<html><body>Henri</body></html>`
+		err := checkInfoPageContent(strings.NewReader(html), "Henri")
+		require.NoError(t, err)
+	})
+
+	t.Run("returns an error if any of the targets are missing from the HTML body", func(t *testing.T) {
+		html := `<html><body>Henri</body></html>`
+		err := checkInfoPageContent(strings.NewReader(html),
+			"Henri",
+			// Should be missing
+			"Today",
+		)
+		require.Errorf(t, err, `target "Today" is missing from body and should produce an error`)
+	})
 }
 
 // CheckTestsEnabled checks we're on the "CI" workflow. If so, returns false, otherwise returns true. We only want these tests to run after a successful deployment.
