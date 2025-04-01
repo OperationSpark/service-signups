b8a4443e34ce27a8c540d51d9d7fb835e7215e4f
Harvey Sanders <sanders.harvey@gmail.com>
2025-04-01 15:20:01 -0400

    Add signup request struct and update webhook handling in Greenlight service

--- greenlight_test.go
+++ greenlight_test.go
@@ -28,18 +28,30 @@ func TestPostWebhook(t *testing.T) {
 			SessionID:         "X5TsABhN94yesyMEi",
 			UserLocation:      "Louisiana",
 			AttendingLocation: "IN_PERSON",
+			SMSOptIn:          true,
 		}
 		mockGreenlightSvr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			// Sends API Key header
 			require.Equal(t, apiKey, r.Header.Get("X-Greenlight-Signup-Api-Key"))
 
 			// POSTs correct JSON body
-			var glReq Signup
+			var glReq signupReq
 			d := json.NewDecoder(r.Body)
 			err := d.Decode(&glReq)
 			require.NoError(t, err)
 
-			require.Equal(t, su, glReq)
+			require.Equal(t, "Bob", glReq.NameFirst)
+			require.Equal(t, "Ross", glReq.NameLast)
+			require.Equal(t, "bross@pbs.org", glReq.Email)
+			require.Equal(t, "555-123-4567", glReq.Cell)
+			require.Equal(t, "instagram", glReq.Referrer)
+			require.Equal(t, "", glReq.ReferrerResponse)
+			require.Equal(t, "is-mar-14-22-12pm", glReq.Cohort)
+			require.Equal(t, "X5TsABhN94yesyMEi", glReq.SessionID)
+			require.Equal(t, "Louisiana", glReq.UserLocation)
+			require.Equal(t, "IN_PERSON", glReq.AttendingLocation)
+			require.Equal(t, false, glReq.SMSOptOut)
+			require.Equal(t, "2022-03-14T17:00:00Z", glReq.StartDateTime.Format(time.RFC3339))
 
 			// Responds with a signup ID
 			resp := signupResp{
